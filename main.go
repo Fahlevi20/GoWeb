62e029d1521c618b0c94119c102157c760f8a527
fahlevi20 <rozakalfi@gmail.com>
2025-04-09 16:10:26 +0700

    add for request to static

--- main.go
+++ main.go
@@ -7,8 +7,10 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+		fmt.Fprintf(w, "Welcome to my website: %s\n", r.URL.Path)
 	})
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static", http.StripPrefix("/static/", fs))
 
 	http.ListenAndServe(":8080", nil)
 }
