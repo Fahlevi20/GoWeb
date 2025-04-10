be001959268f3e378bc59571646f05c2d62569b6
fahlevi20 <rozakalfi@gmail.com>
2025-04-10 15:06:42 +0700

    create connection and query the data using Go from PostgreSQL

---
+++ learn-db-sql/connection.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"flag"
+	"log"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+var pool *sql.DB //
+
+func main() {
+	var err error
+
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	id := flag.Int64("id", 0, "person ID to find")
+	dsn := flag.String("dsn", os.Getenv("DSN"), "connection data source name")
+	flag.Parse()
+
+	if len(*dsn) == 0 {
+		log.Fatal("missing dsn flag")
+	}
+	if *id == 0 {
+		log.Fatal("missing person ID")
+	}
+
+	// Opening a driver typically will not attempt to connect to the database.
+	pool, err = sql.Open("postgres", *dsn)
+	if err != nil {
+		// This will not be a connection error, but a DSN parse error or
+		// another initialization error.
+		log.Fatal("unable to use data source name", err)
+	}
+	defer pool.Close()
+
+	pool.SetConnMaxLifetime(0)
+	pool.SetMaxIdleConns(3)
+	pool.SetMaxOpenConns(3)
+
+	ctx, stop := context.WithCancel(context.Background())
+	defer stop()
+
+	appSignal := make(chan os.Signal, 3)
+	signal.Notify(appSignal, os.Interrupt)
+
+	go func() {
+		<-appSignal
+		stop()
+	}()
+
+	Ping(ctx)
+
+	Query(ctx, *id)
+}
+
+// Ping the database to verify DSN provided by the user is valid and the
+// server accessible. If the ping fails exit the program with an error.
+func Ping(ctx context.Context) {
+	ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
+	defer cancel()
+
+	if err := pool.PingContext(ctx); err != nil {
+		log.Fatalf("unable to connect to database: %v", err)
+	}
+}
+
+// Query the database for the information requested and prints the results.
+// If the query fails exit the program with an error.
+func Query(ctx context.Context, id int64) {
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	rows, err := pool.QueryContext(ctx, "select * from log_tdw;")
+	if err != nil {
+		log.Fatal("query error:", err)
+	}
+	defer rows.Close()
+
+	cols, err := rows.Columns()
+	log.Println("Columns:", cols)
+
+	// Buat slice untuk scan data (tipe []interface{})
+	values := make([]interface{}, len(cols))
+	valuePtrs := make([]interface{}, len(cols))
+
+	for rows.Next() {
+		for i := range cols {
+			valuePtrs[i] = &values[i]
+		}
+
+		if err := rows.Scan(valuePtrs...); err != nil {
+			log.Fatal("scan error:", err)
+		}
+
+		// Cetak data per baris
+		rowData := make(map[string]interface{})
+		for i, col := range cols {
+			val := values[i]
+
+			// Coba convert []byte ke string jika bisa
+			b, ok := val.([]byte)
+			if ok {
+				rowData[col] = string(b)
+			} else {
+				rowData[col] = val
+			}
+		}
+
+		log.Println("Row:", rowData)
+	}
+}
